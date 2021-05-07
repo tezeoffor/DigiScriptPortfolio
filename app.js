3d744e7c16f81691d1abf1f3d5cbfb177b125e98
TG <tezeoffor@gmail.com>
2021-05-08 00:09:18 +0100

    added https

--- app.js
+++ app.js
@@ -11,12 +11,12 @@ app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + "/"));
 
 /* At the top, with other redirect methods before other routes */
-// app.get('*',function(req,res,next){
-//   if(req.headers['x-forwarded-proto']!='https')
-//     res.redirect('https://thedigiscript.com'+req.url)
-//   else
-//     next() /* Continue to other routes if we're not redirecting */
-// })
+app.get('*',function(req,res,next){
+  if(req.headers['x-forwarded-proto']!='https')
+    res.redirect('https://thedigiscript.com'+req.url)
+  else
+    next() /* Continue to other routes if we're not redirecting */
+})
 
 app.get("/", function(req, res){
     res.render("index")
