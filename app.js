a695d1943213fd96377f04dc804b94bdb34f5be9
Christopher <christopher.adeogun@gmail.com>
2021-10-28 10:20:57 +0100

    Uncomment https config

--- app.js
+++ app.js
@@ -15,14 +15,14 @@ app.set('views', path.join(__dirname, '/public/views'))
 app.use(express.static(__dirname + '/public'))
 app.use(express.static(__dirname + '/'))
 
-// /* At the top, with other redirect methods before other routes */
-// if (process.environment != 'dev') {
-//   app.get('*', function (req, res, next) {
-//     if (req.headers['x-forwarded-proto'] != 'https')
-//       res.redirect('https://www.thedigiscript.com' + req.url)
-//     else next() /* Continue to other routes if we're not redirecting */
-//   })
-// }
+/* At the top, with other redirect methods before other routes */
+if (process.environment != 'dev') {
+  app.get('*', function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] != 'https')
+      res.redirect('https://www.thedigiscript.com' + req.url)
+    else next() /* Continue to other routes if we're not redirecting */
+  })
+}
 app.get('/', function (req, res) {
   res.render('index')
 })
